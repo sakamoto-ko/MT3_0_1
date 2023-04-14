7058eed096f7be6a7b4f4109d34dbdeff82b2347
雅 月光 <sakamotokou235@gmail.com>
2023-04-14 11:31:00 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,168 @@
+﻿#include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <assert.h>
+
+#define kWindowWidth 1280
+#define kWindowHeight 720
+
+struct Vector2 {
+	float x;
+	float y;
+};
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Matrix2x2 {
+	float m[2][2];
+};
+struct Matrix3x3 {
+	float m[3][3];
+};
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void VectorScreenPrintf(int x, int y, Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%0.2f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%0.2f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%0.2f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+//減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
+//内積
+float Dot(const Vector3& v1, const Vector3& v2);
+//長さ
+float Length(const Vector3& v);
+//正規化
+Vector3 Normalize(const Vector3& v);
+
+const char kWindowTitle[] = "LE2B_06_サカモトコウスケ_タイトル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		Vector3 v1{ 1.0f,3.0f,-5.0f };
+		Vector3 v2{ 4.0f,-1.0f,2.0f };
+		float k = { 4.0f };
+
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		VectorScreenPrintf(0, 0, resultAdd, " ; Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, " ; Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " ; Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%0.2f ; Subtract", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%0.2f ; Subtract", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " ; Normalize");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
+
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return result;
+}
+
+//減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
+}
+
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 result;
+	result.x = scalar * v.x;
+	result.y = scalar * v.y;
+	result.z = scalar * v.z;
+	return result;
+}
+
+//内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	float result;
+	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	return result;
+}
+
+//長さ
+float Length(const Vector3& v) {
+	float result;
+	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return result;
+}
+
+//正規化
+Vector3 Normalize(const Vector3& v) {
+	Vector3 result;
+	float a = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	if (a != 0) {
+		result.x = v.x / a;
+		result.y = v.y / a;
+		result.z = v.z / a;
+	}
+	return result;
+}
